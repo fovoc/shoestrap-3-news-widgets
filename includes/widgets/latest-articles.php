f8ace1b721dede07c6895dbab143c1f23094983b
Aristeides Stathopoulos <aristath@gmail.com>
2013-12-12 18:53:55 +0200

    Added title size option

--- includes/widgets/latest-articles.php
+++ includes/widgets/latest-articles.php
@@ -27,18 +27,19 @@ class shoestrap_news_widget_latest_articles extends WP_Widget {
 
 		extract( $args );
 
-		$title          = apply_filters( 'widget_title', $instance['title'] );
-		$post_type      = $instance['post_type'];
-		$taxonomy       = $instance['taxonomy'];
-		$term           = $instance['term'];
-		$per_page       = $instance['per_page'];
-		$offset         = $instance['offset'];
-		$thumb          = $instance['thumb'];
-		$thumb_float    = $instance['thumb_float'];
-		$thumb_width    = $instance['thumb_width'];
-		$thumb_height   = $instance['thumb_height'];
-		$excerpt_length = $instance['excerpt_length'];
-		$more_text      = $instance['more_text'];
+		$title           = apply_filters( 'widget_title', $instance['title'] );
+		$post_type       = $instance['post_type'];
+		$taxonomy        = $instance['taxonomy'];
+		$term            = $instance['term'];
+		$per_page        = $instance['per_page'];
+		$offset          = $instance['offset'];
+		$thumb           = $instance['thumb'];
+		$thumb_float     = $instance['thumb_float'];
+		$thumb_width     = $instance['thumb_width'];
+		$thumb_height    = $instance['thumb_height'];
+		$excerpt_length  = $instance['excerpt_length'];
+		$more_text       = $instance['more_text'];
+		$post_title_size = $instance['post_title_size'];
 
 		echo $before_widget;
 
@@ -66,7 +67,8 @@ class shoestrap_news_widget_latest_articles extends WP_Widget {
 					$instance['thumb_width'],
 					$instance['thumb_height'],
 					$instance['excerpt_length'],
-					$instance['more_text']
+					$instance['more_text'],
+					$instance['post_title_size']
 				);
 			?>
 		</div>
@@ -78,18 +80,19 @@ class shoestrap_news_widget_latest_articles extends WP_Widget {
 		$instance = $old_instance;
 
 		/* Strip terms (if needed) and update the widget settings. */
-		$instance['title']          = strip_tags( $new_instance['title'] );
-		$instance['post_type']      = strip_tags( $new_instance['post_type'] );
-		$instance['taxonomy']       = strip_tags( $new_instance['taxonomy'] );
-		$instance['term']           = strip_tags( $new_instance['term'] );
-		$instance['per_page']       = strip_tags( $new_instance['per_page'] );
-		$instance['offset']         = strip_tags( $new_instance['offset'] );
-		$instance['thumb']          = isset( $new_instance['thumb'] );
-		$instance['thumb_float']    = isset( $new_instance['thumb_float'] );
-		$instance['thumb_width']    = strip_tags( $new_instance['thumb_width'] );
-		$instance['thumb_height']   = strip_tags( $new_instance['thumb_height'] );
-		$instance['excerpt_length'] = strip_tags( $new_instance['excerpt_length'] );
-		$instance['more_text']      = strip_tags( $new_instance['more_text'] );
+		$instance['title']           = strip_tags( $new_instance['title'] );
+		$instance['post_type']       = strip_tags( $new_instance['post_type'] );
+		$instance['taxonomy']        = strip_tags( $new_instance['taxonomy'] );
+		$instance['term']            = strip_tags( $new_instance['term'] );
+		$instance['per_page']        = strip_tags( $new_instance['per_page'] );
+		$instance['offset']          = strip_tags( $new_instance['offset'] );
+		$instance['thumb']           = isset( $new_instance['thumb'] );
+		$instance['thumb_float']     = isset( $new_instance['thumb_float'] );
+		$instance['thumb_width']     = strip_tags( $new_instance['thumb_width'] );
+		$instance['thumb_height']    = strip_tags( $new_instance['thumb_height'] );
+		$instance['excerpt_length']  = strip_tags( $new_instance['excerpt_length'] );
+		$instance['more_text']       = strip_tags( $new_instance['more_text'] );
+		$instance['post_title_size'] = strip_tags( $new_instance['post_title_size'] );
 
 		return $instance;
 	}
@@ -97,18 +100,19 @@ class shoestrap_news_widget_latest_articles extends WP_Widget {
 	function form( $instance ) {
 
 		$defaults = array(
-			'title'          => 'Latest Articles',
-			'post_type'      => 'post',
-			'taxonomy'       => 'category',
-			'term'           => 'shoestrap_nw_all_terms',
-			'per_page'       => 5,
-			'offset'         => 0,
-			'thumb'          => true,
-			'thumb_float'    => 1,
-			'thumb_width'    => 150,
-			'thumb_height'   => 100,
-			'excerpt_length' => 20,
-			'more_text'      => __( 'Read More', 'shoestrap_nw' )
+			'title'           => 'Latest Articles',
+			'post_type'       => 'post',
+			'taxonomy'        => 'category',
+			'term'            => 'shoestrap_nw_all_terms',
+			'per_page'        => 5,
+			'offset'          => 0,
+			'thumb'           => true,
+			'thumb_float'     => 1,
+			'thumb_width'     => 150,
+			'thumb_height'    => 100,
+			'excerpt_length'  => 20,
+			'more_text'       => __( 'Read More', 'shoestrap_nw' ),
+			'post_title_size' => 'h4'
 		);
 
 		$instance = wp_parse_args( ( array ) $instance, $defaults ); ?>
@@ -182,6 +186,20 @@ class shoestrap_news_widget_latest_articles extends WP_Widget {
 				<td><input id="<?php echo $this->get_field_id( 'per_page' ); ?>" name="<?php echo $this->get_field_name( 'offset' ); ?>" value="<?php echo $instance['offset']; ?>" type="number" /></td>
 			</tr>
 
+			<tr style="margin: 10px 0;">
+				<td><?php _e( 'Post Title Size:','shoestrap_nw'); ?></td>
+				<td>
+					<input class="radio" type="radio" <?php if ( $instance['post_title_size'] == 'h3') { ?>checked <?php } ?>name="<?php echo $this->get_field_name( 'post_title_size' ); ?>" value="h3" id="<?php echo $this->get_field_id( 'post_title_size' ); ?>_h3" />
+					<?php _e( 'Large (H3)', 'shoestrap_nw' ); ?>
+					<br />
+					<input class="radio" type="radio" <?php if ( $instance['post_title_size'] == 'h4') { ?>checked <?php } ?>name="<?php echo $this->get_field_name( 'post_title_size' ); ?>" value="h4" id="<?php echo $this->get_field_id( 'post_title_size' ); ?>_h4" />
+					<?php _e( 'Normal (H4)', 'shoestrap_nw' ); ?>
+					<br />
+					<input class="radio" type="radio" <?php if ( $instance['post_title_size'] == 'h5') { ?>checked <?php } ?>name="<?php echo $this->get_field_name( 'post_title_size' ); ?>" value="h5" id="<?php echo $this->get_field_id( 'post_title_size' ); ?>_h5" />
+					<?php _e( 'Small (H5)', 'shoestrap_nw' ); ?>
+					<br />
+			</tr>
+
 			<tr>
 				<td><?php _e( 'Excerpt Length','shoestrap_nw'); ?></td>
 				<td><input id="<?php echo $this->get_field_id( 'excerpt_length' ); ?>" name="<?php echo $this->get_field_name( 'excerpt_length' ); ?>" value="<?php echo $instance['excerpt_length']; ?>" type="number" /></td>
@@ -224,7 +242,7 @@ class shoestrap_news_widget_latest_articles extends WP_Widget {
 	}
 }
 
-function shoestrap_nw_posts_loop( $post_type = 'post', $taxonomy = '', $term = '', $posts_per_page = 5, $offset = 0, $thumb = false, $thumb_float = true, $thumb_width = 150, $thumb_height = 100, $excerpt_length = 20, $read_more_text = '...' ) {
+function shoestrap_nw_posts_loop( $post_type = 'post', $taxonomy = '', $term = '', $posts_per_page = 5, $offset = 0, $thumb = false, $thumb_float = true, $thumb_width = 150, $thumb_height = 100, $excerpt_length = 20, $read_more_text = '...', $post_title_size = 'h4' ) {
 
 	// Set-Up the taxonomy query
 	if ( $term != 'shoestrap_nw_all_terms' ) :
@@ -268,7 +286,23 @@ function shoestrap_nw_posts_loop( $post_type = 'post', $taxonomy = '', $term = '
 			<?php if ( $thumb && has_post_thumbnail() ) : ?>
 				<?php if ( !$thumb_float ) : ?>
 					<div class="media-body">
-						<a href="<?php the_permalink(); ?>"><h4 class="media-heading"><?php the_title(); ?></h4></a>
+						<a href="<?php the_permalink(); ?>">
+							<?php if ( $post_title_size == 'h3' ) : ?>
+								<h3 class="media-heading">
+							<?php elseif ( $post_title_size == 'h4' ) : ?>
+								<h4 class="media-heading">
+							<?php else : ?>
+								<h5 class="media-heading">
+							<?php endif; ?>
+								<?php the_title(); ?></h4>
+							<?php if ( $post_title_size == 'h3' ) : ?>
+								</h3>
+							<?php elseif ( $post_title_size == 'h4' ) : ?>
+								</h4>
+							<?php else : ?>
+								</h5>
+							<?php endif; ?>
+						</a>
 				<?php endif; ?>
 				<a <?php echo $thumb_class; ?> href="<?php the_permalink(); ?>">
 					<img class="media-object" src="<?php echo $image['url']; ?>" alt="<?php the_title(); ?>">
@@ -278,7 +312,23 @@ function shoestrap_nw_posts_loop( $post_type = 'post', $taxonomy = '', $term = '
 				<?php echo shoestrap_nw_excerpt( $excerpt_length, $read_more_text ); ?>
 			<?php else : ?>
 				<div class="media-body">
-					<a href="<?php the_permalink(); ?>"><h4 class="media-heading"><?php the_title(); ?></h4></a>
+					<a href="<?php the_permalink(); ?>">
+						<?php if ( $post_title_size == 'h3' ) : ?>
+							<h3 class="media-heading">
+						<?php elseif ( $post_title_size == 'h4' ) : ?>
+							<h4 class="media-heading">
+						<?php else : ?>
+							<strong>
+						<?php endif; ?>
+							<?php the_title(); ?></h4>
+						<?php if ( $post_title_size == 'h3' ) : ?>
+							</h3>
+						<?php elseif ( $post_title_size == 'h4' ) : ?>
+							</h4>
+						<?php else : ?>
+							</strong>
+						<?php endif; ?>
+					</a>
 					<?php echo shoestrap_nw_excerpt( $excerpt_length, $read_more_text ); ?>
 			<?php endif; ?>
 			</div>
