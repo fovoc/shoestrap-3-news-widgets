46425104b430dbe674afd00cd5bc353fcf299827
fovoc <kalliris.d@gmail.com>
2014-07-23 20:10:09 +0300

    Shoestrap isn't required

--- s3-news-widgets.php
+++ s3-news-widgets.php
@@ -26,14 +26,31 @@ if ( ! defined( 'S3NW_PLUGIN_FILE' ) ) {
 /**
  * Include plugin files
  */
-function ssnw_include_files() {
-	include_once( S3NW_PLUGIN_DIR . 'includes/widget.posts-query.php' );
-	include_once( S3NW_PLUGIN_DIR . 'includes/functions.loop.php' );
-	include_once( S3NW_PLUGIN_DIR . 'includes/functions.excerpts.php' );
-}
-add_action( 'shoestrap_include_files', 'ssnw_include_files' );
+
+include_once( S3NW_PLUGIN_DIR . 'includes/widget.posts-query.php' );
+include_once( S3NW_PLUGIN_DIR . 'includes/functions.loop.php' );
+include_once( S3NW_PLUGIN_DIR . 'includes/functions.excerpts.php' );
+include_once( S3NW_PLUGIN_DIR . 'includes/class-Shoestrap_Image.php' );
 
 function shoestrap_news_widgets() {
 	register_widget( 'shoestrap_news_widget_latest_articles' );
 }
 add_action( 'widgets_init', 'shoestrap_news_widgets' );
+
+/**
+ * Enqueue styles
+ */
+function shoestrap_news_widgets_styles() {
+	wp_enqueue_style( 'style', S3NW_PLUGIN_URL . 'assets/style.css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'shoestrap_news_widgets_styles' );
+
+/**
+ * Enqueue admin script
+ */
+function shoestrap_news_widgets_admin_script() {
+	wp_enqueue_script( 'script', S3NW_PLUGIN_URL . 'assets/script.js', array(), '1.0.0', true );
+}
+
+add_action( 'admin_enqueue_scripts', 'shoestrap_news_widgets_admin_script' );
